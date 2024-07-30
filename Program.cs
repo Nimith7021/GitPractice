2ee590b04b8685b154eeae10f05bdd5815f1ae40
Sreerag Rajesh Babu <Sreerag.Babu@asl.mum.sst>
2024-07-30 12:14:58 +0530

    v5

--- Program.cs
+++ Program.cs
@@ -9,17 +9,19 @@ namespace AgeChecker
             Player[] playerDetails = new playerDetails[] {
 
             new playerDetails(100,"Adam",20),
-            new playerDetails(101,"Ajesh"),
-            new playerDetails(102,"Damian",38),
-            new playerDetails(103,"Arlo",29),
+            new playerDetails2(101,"Ajesh"),
+            new playerDetaedhgyeiils(102,"Damian",38),
+            new playersDetails(103,"Arlo",29),
             new playerDetails(104,"James",36),
+            new playerDetairjghkrls(105,"James",36),
+            
 
             };
 
             Player maxPlayer = playerDetails.WhoIsElder(players);
 
             Player maxPlayer = Player.WhoIsElder(players);
-            Console.WriteLine(Player with max age);
+            Console.WriteLine("Player with max age");
             Console.WriteLine(maxPlayer);
         }
     }
